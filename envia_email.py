5c4c7ffc14d8b86fecb20e883df39db37edc420a
Pedro Mileipp <poncianopedro70@gmail.com>
2024-08-23 20:10:00 -0300

    adiciona config.ini e muda envia_email

--- envia_email.py
+++ envia_email.py
@@ -1,18 +1,22 @@
 import smtplib
 import email.message
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 def enviar_email(assunto, corpo):  
 
     msg = email.message.Message()
     msg['Subject'] = assunto
-    msg['From'] = 'pedromileipp2@gmail.com'
-    msg['To'] = 'pedromileipp@gmail.com'
-    password = 'senha' 
+    msg['From'] = config['EMAIL']['Remetente']
+    msg['To'] = config['EMAIL']['Destino']
+    password = config['SMTP']['Senha'] 
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo)
 
-    s = smtplib.SMTP('smtp.gmail.com: 587')
+    s = smtplib.SMTP(config['SMTP']['Porta'])
     s.starttls()
     s.login(msg['From'], password)
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
-    print('Email enviado')
+    
